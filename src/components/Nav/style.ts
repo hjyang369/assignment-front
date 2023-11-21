4fad4b74ad3f59783a140a3bae26fd078c222aa9
hjyang369 <ghlwls6@gmail.com>
2023-11-21 18:36:14 +0900

    feat: bottom 레이아웃 완성

--- src/components/Nav/style.ts
+++ src/components/Nav/style.ts
@@ -2,11 +2,15 @@ import styled from "styled-components";
 
 export const S = {
   Container: styled.div`
+    position: fixed;
+    top: 0;
+    z-index: 1;
     background-color: #ffffff;
     display: flex;
     align-items: center;
     justify-content: start;
     gap: 1rem;
+    width: 375px;
     height: 60px;
     box-shadow: rgba(33, 35, 38, 0.1) 0px 10px 10px -10px;
     padding: 12px 20px;
