cdd20b470edae22f46c74ff6c4f7fa4c924ba919
hjyang369 <ghlwls6@gmail.com>
2023-11-21 17:54:43 +0900

    feat: nav 레이아웃 완성

---
+++ src/components/Bottom/index.tsx
@@ -0,0 +1,5 @@
+import { S } from "./style";
+
+export default function Bottom() {
+  return <S.Container></S.Container>;
+}
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,3 +1,22 @@
+import {
+  faCalendarCheck,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { S } from "./style";
+import Tag from "components/Tag";
+
+const navIcon = [
+  { id: 1, icon: faMagnifyingGlass, text: "전체 헤드라인" },
+  { id: 2, icon: faCalendarCheck, text: "전체 날짜" },
+  { id: 3, icon: undefined, text: "전체 국가" },
+];
+
 export default function Nav() {
-  return <div></div>;
+  return (
+    <S.Container>
+      {navIcon.map((icon) => {
+        return <Tag key={icon.id} iconName={icon.icon} text={icon.text} />;
+      })}
+    </S.Container>
+  );
 }
